be81af79b9c60fbd178ad6d7200c4d773fe319fd
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-10-12 20:54:11 +0300

    add explanation for simplefactory

--- DesignPatterns.SimpleFactory/Program.cs
+++ DesignPatterns.SimpleFactory/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DesignPatterns.SimpleFactory.Factory;
 
+// Simple factory simply generates an instance for client
+// without exposing any instantiation logic to the client
 var door = DoorFactory.MakeDoor(80, 30);
 
 Console.WriteLine($"Height of door is: {door.GetHeight()}");
