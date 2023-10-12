22b0b9f333581419d9a1853da2590ab1a3a19b84
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-10-12 20:55:14 +0300

    fix import

--- DesignPatterns.FactoryMethod/Program.cs
+++ DesignPatterns.FactoryMethod/Program.cs
@@ -1,5 +1,7 @@
-﻿using DesignPatterns.FactoryMethod.Conrete;
+﻿using DesignPatterns.FactoryMethod.Abstract;
+using DesignPatterns.FactoryMethod.Conrete;
 
+// It provides a way to delegate the instantiation logic to child classes.
 var devManager = new DevelopmentManager();
 devManager.TakeInterview(); // asking about design patterns
 
